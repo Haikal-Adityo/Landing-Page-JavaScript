f151a003e1c03842e08293b84ef94cabebdb2542
Haikal-Adityo <haikaladityo@gmail.com>
2023-05-25 19:00:35 +0700

    19:00 25/5/2023
    Add Item Working

--- Ocean.js
+++ Ocean.js
@@ -41,13 +41,51 @@ btnClose.addEventListener('click', () => {
 
 const btnCalculate = document.querySelector("#btn-calculate");
 const btnAdd = document.querySelector("#btn-add");
-var hasil = 0;
-
-const item1 = document.querySelector("#item1");
-const item2 = document.querySelector("#item2");
-const item3 = document.querySelector("#item3");
+var isAdded = false;
 
 btnCalculate.addEventListener('click', () => {
-    hasil = item1.value + item2.value + item3.value;
+
+    const result = document.querySelector(".result");
+    const item1 = parseInt(document.querySelector("#item1").value);
+    const item2 = parseInt(document.querySelector("#item2").value);
+    const item3 = parseInt(document.querySelector("#item3").value);
+
+    if (isAdded) {
+
+        const item4 = parseInt(document.querySelector("#item4").value);
+        const hasil = (item1 + item2 + item3 + item4) * 365;
+        result.innerHTML = "<br> Total trash per year: " + hasil ;
+    }
+    else {
+        const hasil = (item1 + item2 + item3) * 365;
+        result.innerHTML = "<br> Total trash per year: " + hasil ;
+    }
+
+})
+
+btnAdd.addEventListener('click', () => {
+    
+    const result = document.querySelector(".add-box");
+    let box = '<input type="text" name="add item" id="add" size="20" maxlength="20"></input>'
+    let txt = '<p style="color: red; font-size:10px; margin-top: 5px">*You can only add one item</p>'
+    let btn = '<button type="button" id="btn-save">Save</button>'
+
+    result.innerHTML = box + txt + btn;
+    
+    const btnSave = document.querySelector("#btn-save");
+
+    btnSave.addEventListener('click', () => {
+
+        const addedItem = document.querySelector("#add");
+        const result = document.querySelector(".added-box");
+        
+        let label ="<label>" + addedItem.value + "</label>"
+        let box = '<input type="text" name="item" id="item4" size="10" maxlength="10"></input>'
+
+        isAdded = true;
+
+        result.innerHTML = label + box;
+        addedItem.value = '';
+    });
 
 })
